6c24db53884a31890954310ea6324a30544aa1bb
Antonio Sisinni <92176296+ANSIoso@users.noreply.github.com>
2022-11-26 16:14:53 +0100

    inserimento di alcuni firewall

--- architettura/F1/etc/network/firewall.sh
+++ architettura/F1/etc/network/firewall.sh
@@ -1,1 +1,26 @@
 #!/usr/bin/sh
+
+iptable -F
+iptable -X
+
+iptable INPUT DROP
+iptable OUTPUT DROP
+iptable FORWARD DROP
+
+iptable -N green_all
+iptable -N all_green
+
+iptable -A FORWARD -s 10.0.0.0/23 -i eth2 -j green_all
+iptable -A FORWARD -d 10.0.0.0/23 -o eth2 -j all_green
+
+iptable -A green_all ACCEPT
+iptable -A all_green -m state --state ESTABILISHED,RELATED all_green ACCEPT
+
+iptable -N dmz_red 
+iptable -N red_dmz
+
+iptable -A FORWARD -s 10.0.3.0/26 -i eth2 -d 10.0.2.0/24 -o eth1 -j dmz_red
+iptable -A FORWARD -s 10.0.2.0/24 -i eth1 -d 10.0.3.0/26 -o eth2 -j red_dmz
+
+iptable -A dmz_red ACCEPT
+iptable -A red_dmz -m state --state ESTABILISHED,RELATED ACCEPT
